6d0a6ae6c637465e8ad4a0e6a4ec2af792ca8776
Yiming Pan <56082650+YimingPan-Code@users.noreply.github.com>
2021-11-16 14:39:54 -0500

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -137,3 +137,6 @@ gsql -g RJF "run loading job load_job_relationship using
 rel=\"/app/tigergraph/rel.csv\""
 
 gsql -g RJF "run loading job load_job_relationship"
+
+gsql -g RJF "run loading job load_job_TG_customer using 
+g=\"/app/tigergraph/customer_edge.csv\""
